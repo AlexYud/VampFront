6b630ed9a7d2a5c647916816def85b1e9f736f2c
Alexandre Yudi <alexyud@hotmail.com>
2023-11-29 09:43:01 -0300

    Preparing for deploy

--- src/components/socket.js
+++ src/components/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client'
 
 // export default io("https://vamp-api.herokuapp.com/", { transports: ["websocket"] })
-export default io("https://alpha-vamp-api.onrender.com:3000/", { transports: ["websocket"] })
+export default io("https://alpha-vamp-api.onrender.com/", { transports: ["websocket"] })
